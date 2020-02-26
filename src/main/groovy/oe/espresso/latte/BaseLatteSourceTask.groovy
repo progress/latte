e1b7d4080761cef2eff59b8e45afe90e46401b77
Matt Baker <mbaker@progress.com>
2020-02-26 17:53:47 -0500

    add ability to compile and run with graphics mode

--- src/main/groovy/oe/espresso/latte/BaseLatteSourceTask.groovy
+++ src/main/groovy/oe/espresso/latte/BaseLatteSourceTask.groovy
@@ -1,5 +1,5 @@
 /**
-  Copyright © 2019 Progress Software Corporation and/or its subsidiaries or affiliates. All Rights Reserved.
+  Copyright © 2019,2020 Progress Software Corporation and/or its subsidiaries or affiliates. All Rights Reserved.
 */
 
 package oe.espresso.latte
@@ -25,6 +25,13 @@ abstract class BaseLatteSourceTask extends SourceTask {
     */
     @Input @Optional
     File dlcHome = ext.dlcHome
+
+    /**
+    use prowin32 or prowin (whichever is appropriate),
+    otherwise _progress
+    */
+    @Input @Optional
+    Boolean graphicalMode = ext.graphicalMode    
 
     /**
         get value of DlcHome
